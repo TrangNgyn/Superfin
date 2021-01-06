d13650e5a861141a34ef572cc249cb58a7a2e524
steamedHams11 <69377385+steamedHams11@users.noreply.github.com>
2021-01-06 15:13:23 +1000

    Finished: EmailRequest, ReenterPassword, ResetPasswordConfirmation pages

--- client/ReactProject/src/App/App.js
+++ client/ReactProject/src/App/App.js
@@ -1,6 +1,9 @@
 import './App.less';
 import "../_assets/CSS/style.scss";
 import Login from '../Pages/Login/Login';
+import EmailRequest from '../Pages/EmailRequest/EmailRequest';
+import ResetPasswordConfirmation from '../Pages/ResetPasswordConfirmation/ResetPasswordConfirmation';
+import ReenterPassword from '../Pages/ReenterPassword/ReenterPassword';
 import Navbar from '../SharedComponents/Navbar/Navbar';
 import FooterMain from '../SharedComponents/Footer/FooterMain';
 
@@ -14,7 +17,7 @@ const App = () => (
    </div>
 
    <div className="Website-Body">
-      <Login />
+      <ReenterPassword />
    </div>
 
  <div className="Website-Footer">
