4b440b6a573a1447c614e203860955a96ce2994c
thaithinh1312 <thaiquangthinh1312@gmail.com>
2021-01-24 11:55:09 +1100

    checkout-series-finalize

--- client/ReactProject/src/App/App.js
+++ client/ReactProject/src/App/App.js
@@ -13,6 +13,10 @@ import AboutUS from '../Pages/AboutUs/AboutUs';
 import EmailRequest from '../Pages/EmailRequest/EmailRequest';
 import ResetPasswordConfirmation from '../Pages/ResetPasswordConfirmation/ResetPasswordConfirmation';
 import TermsConditions from '../Pages/TermsConditions/TermsConditions';
+import CheckoutShipping from '../Pages/Checkout/CheckoutShipping';
+import CheckoutSecurePayment from '../Pages/Checkout/CheckoutSecurePayment';
+import CheckoutReviewOrder from '../Pages/Checkout/CheckoutReviewOrder';
+import CheckoutOrderComplete from '../Pages/Checkout/CheckoutOrderComplete';
 
 
 
@@ -46,22 +50,25 @@ const App = () => (
          </div>
 
          <div className="Website-Body">
-            <Switch>
-               {/*<CustomerRoute path="/" component = {Homepage} exact />*/}           {/* This is how to use the private routes. Do not use them yet */}
-               <Route exact path="/"><Homepage /></Route>
+         <Switch>
+            {/*<CustomerRoute path="/" component = {Homepage} exact />*/}           {/* This is how to use the private routes. Do not use them yet */}
+            <Route exact path="/"><Homepage /></Route>
 
-               <Route exact path="/products"> <Products /></Route>
-               <Route exact path="/products/:browseBy"><Products /></Route>
-               <Route path="/products/:browseBy/:browseByGroup"><Products /></Route>          {/*Undecided on Products page path*/}
-
-               <Route path="/deliveryDispatch"> <DeliveryDispatch /></Route>
-               <Route path="/contactUs"> <ContactUs /></Route>
-               <Route path="/aboutUs"><AboutUS /></Route>
-               <Route path="/login"><Login /></Route>
-               <Route path="/emailRequest"><EmailRequest /></Route>
-               <Route path="/resetPasswordConfirmation"><ResetPasswordConfirmation /></Route>
-               <Route path="/TermsConditions"><TermsConditions /></Route>
-            </Switch>
+            <Route exact path="/products"> <Products /></Route>
+            <Route exact path="/products/:browseBy"><Products /></Route>
+            <Route path="/products/:browseBy/:browseByGroup"><Products /></Route>          {/*Undecided on Products page path*/}
+            <Route path="/deliveryDispatch"> <DeliveryDispatch /></Route>
+            <Route path="/contactUs"> <ContactUs /></Route>
+            <Route path="/aboutUs"><AboutUS /></Route>
+            <Route path="/login"><Login /></Route>
+            <Route path="/emailRequest"><EmailRequest /></Route>
+            <Route path="/resetPasswordConfirmation"><ResetPasswordConfirmation /></Route>
+            <Route path="/TermsConditions"><TermsConditions /></Route>
+            <Route path="/CheckoutShipping"><CheckoutShipping /></Route>
+            <Route path="/CheckoutSecurePayment"><CheckoutSecurePayment /></Route>
+            <Route path="/CheckoutReviewOrder"><CheckoutReviewOrder /></Route>
+            <Route path="/CheckoutOrderComplete"><CheckoutOrderComplete /></Route>
+         </Switch>
          </div>
 
          <div className="Website-Footer">
--- client/ReactProject/src/Pages/Checkout/CheckoutReviewOrder.js
+++ client/ReactProject/src/Pages/Checkout/CheckoutReviewOrder.js
@@ -1,7 +1,8 @@
 import '../../_assets/CSS/pages/CheckoutReviewOrder.css';
 import { Typography, Button, Row, Col, Steps } from 'antd';
 import {EditFilled} from '@ant-design/icons';
-
+import { Link } from 'react-router-dom';
+import img from "../../_assets/Images/Temp_Images/product_image_1.jpg"
 
 
 
@@ -31,7 +32,7 @@ const CheckoutReviewOrder = () =>{
         <div id="checkout-review-order-info">
         <Row>
         <Col span={12}><Title level={4}>Shipping Address</Title></Col>
-        <Col offset={10}><Button type="text"><EditFilled /></Button></Col>
+        <Col offset={10}><Button type="text"><Link to="/CheckoutShipping"><EditFilled /></Link></Button></Col>
         </Row>
         <Row><b>Name:</b>  A</Row>
         <Row><b>Address:</b>  23 Denison Rd, New South Wales 2500 Australia</Row>
@@ -49,8 +50,9 @@ const CheckoutReviewOrder = () =>{
         <Col span={5}><div style={{textAlign: "center",fontWeight:"bold"}}>Quantity</div></Col>
         <Col span={5}><div style={{textAlign: "right",paddingRight: "10px",fontWeight:"bold"}}>Total</div></Col>
         </Row>
-        <Row>
-        <Col span={9}><img src="../../_assets/Images/Temp_Images/product_image_2.jpg"></img>Brown Bag with Handles</Col>
+        <Row justify="space-around" align="middle">
+        <Col span={4}><img src={img} width="100%" height="100%"/></Col>
+        <Col span={5}>Brown Bag with Handles</Col>
         <Col span={5}><div style={{textAlign: "center"}}>100.00</div></Col>
         <Col span={5}><div style={{textAlign: "center"}}>1</div></Col>
         <Col span={5}><div style={{textAlign: "right",paddingRight: "10px"}}>100.00</div></Col>
@@ -72,12 +74,12 @@ const CheckoutReviewOrder = () =>{
         </Row>
         <Row>
         <Col offset={11} span={7}><div style={{textAlign: "left",fontWeight:"bold"}}>Total:</div></Col>
-        <Col span={6}><div style={{textAlign: "right"}}>$242.00</div></Col>
+        <Col span={6}><div style={{textAlign: "right",fontWeight:"bold"}}>$242.00</div></Col>
         </Row>
         </div>
 
         <div id="checkout-review-order-button">
-        <Button type="primary">Place Order</Button>
+        <Button type="primary"><Link to="/CheckoutOrderComplete">Place Order</Link></Button>
         </div>
 
         </div>
