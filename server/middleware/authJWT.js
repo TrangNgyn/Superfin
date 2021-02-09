3e7cd45f311dc179a2dab0067d0461e244ca8893
Oliver Dunn <oged97@gmail.com>
2021-02-09 13:37:06 +1100

    Revert "added concurrently script, client install script, client run script"
    This reverts commit 5bb1ff7fa8588b70fda5d486e4406f051a638479.

--- server/middleware/authJWT.js
+++ server/middleware/authJWT.js
@@ -1,60 +1,58 @@
-// const jwt = require("jsonwebtoken");
-// const config = require("../config/auth_config.js");
-// const db = require("../models");
-// const Customer = db.customer;
-// const Role = db.role;
+const jwt = require("jsonwebtoken");
+const config = require("../config/auth_config.js");
+const db = require("../models");
+const Customer = db.customer;
+const Role = db.role;
 
-// verifyToken = (req, res, next) => {
-//   let token = req.headers["x-access-token"];
+verifyToken = (req, res, next) => {
+  let token = req.headers["x-access-token"];
 
-//   if (!token) {
-//     return res.status(403).send({ message: "No token provided!" });
-//   }
+  if (!token) {
+    return res.status(403).send({ message: "No token provided!" });
+  }
 
-//   jwt.verify(token, config.secret, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).send({ message: "Unauthorized!" });
-//     }
-//     req.userId = decoded.id;
-//     next();
-//   });
-// };
+  jwt.verify(token, config.secret, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ message: "Unauthorized!" });
+    }
+    req.userId = decoded.id;
+    next();
+  });
+};
 
-// // check for admin privileges
-// exports.isAdmin = (req, res, next) => {
-//   Customer.findById(req.userId).exec((err, user) => {
-//     if (err) {
-//       res.status(500).send({ message: err });
-//       return;
-//     }
+// check for admin privileges
+exports.isAdmin = (req, res, next) => {
+  Customer.findById(req.userId).exec((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
 
-//     Role.find(
-//       {
-//         _id: { $in: user.roles }
-//       },
-//       (err, roles) => {
-//         if (err) {
-//           res.status(500).send({ message: err });
-//           return;
-//         }
+    Role.find(
+      {
+        _id: { $in: user.roles }
+      },
+      (err, roles) => {
+        if (err) {
+          res.status(500).send({ message: err });
+          return;
+        }
 
-//         for (let i = 0; i < roles.length; i++) {
-//           if (roles[i].name === "admin") {
-//             next();
-//             return;
-//           }
-//         }
+        for (let i = 0; i < roles.length; i++) {
+          if (roles[i].name === "admin") {
+            next();
+            return;
+          }
+        }
 
-//         res.status(403).send({ message: "Require Admin Role!" });
-//         return;
-//       }
-//     );
-//   });
-// };
+        res.status(403).send({ message: "Require Admin Role!" });
+        return;
+      }
+    );
+  });
+};
 
-// exports.authJwt = () => {
-//   verifyToken,
-//   isAdmin
-// };
-
-
+exports.authJwt = () => {
+  verifyToken,
+  isAdmin
+};
