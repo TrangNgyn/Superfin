db73d7a04ab638ed0a5a9c134a0413b7ccd5599d
kathniev-source <kathrynviennguyen@gmail.com>
2021-02-19 21:26:33 +1100

    Fixed navbar + footer + body alignment and responsiveness
    - Navbar and Footer now always get elevated with z-index 99
    - Body is elevated to z-index 90
    - Footer for screen > 575px will now stick to the bottom
    - Body now has 200px of after content to make the content seeable when
      scroll to the bottom
    - Minor fixes to CSS for navbar, body and footer
    - Removed ContactUs.css
    - Added loremipsum to About Us for testing
    - Adjustment to ContactUs.js to use common element instead of using
      separate CSS

--- client/ReactProject/src/Pages/AboutUs/AboutUs.js
+++ client/ReactProject/src/Pages/AboutUs/AboutUs.js
@@ -1,14 +1,21 @@
-import '../../_assets/CSS/pages/AboutUs.css';
 const AboutUs = () =>{
     return (
-      <body>
-      <div id="about-us-window">
-        About our company
-      </div>
-      <div id="about-us-text">
-        Placeholder text
-      </div>
-      </body>
+      <>
+        <div className="page-title-holder fill center-text">
+          <h2>About Our Company</h2>
+        </div>
+        <div id="about-us-text" className="container">
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas id nulla quis erat tristique tristique. Curabitur nec justo nibh. Fusce finibus felis et leo suscipit, sed convallis massa euismod. Fusce quis ligula rutrum, molestie massa nec, accumsan sapien. Nullam massa libero, posuere quis turpis nec, pellentesque porta erat. Donec eu tempor magna. Maecenas ligula elit, venenatis ac suscipit sit amet, ultricies in orci. Pellentesque a felis eleifend, imperdiet nisl non, ornare augue. Suspendisse cursus magna quis ligula pellentesque tristique. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aliquam congue metus vel magna fermentum, non venenatis nibh semper.
+
+  Vestibulum ultrices molestie erat, a suscipit arcu tempus quis. Donec elementum placerat justo, sit amet porta quam tempus sed. Sed auctor ex velit, viverra convallis mi gravida et. Aenean tincidunt id velit eget eleifend. Nunc accumsan felis id turpis laoreet, id ornare velit consectetur. Donec non libero a massa laoreet interdum. Fusce vulputate sollicitudin sodales. Curabitur tempus, mauris vitae mollis convallis, ligula est auctor ex, vel accumsan tortor diam vitae lectus. Integer nisi nibh, consequat et enim id, aliquet congue felis. Maecenas sit amet lacus ullamcorper, varius enim ac, feugiat ex. Proin pellentesque luctus neque, eu tincidunt diam ultricies ac. Fusce in sapien in mi iaculis suscipit vel ut sem. Phasellus diam mi, maximus et ex sed, facilisis fermentum arcu. Nulla eu eros magna. Phasellus vitae feugiat ligula, sed tristique eros. Duis velit ex, tincidunt nec nunc hendrerit, feugiat dignissim tellus.
+
+  Aenean pellentesque fringilla massa, congue venenatis velit tincidunt nec. Nulla facilisi. Ut at ante eu lorem viverra cursus non nec purus. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Ut eleifend ipsum leo, lacinia auctor enim finibus sed. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec id varius turpis. Phasellus vitae posuere diam, quis vestibulum mauris. Aliquam sollicitudin libero in tellus ornare imperdiet. Nam dictum erat odio, lacinia dignissim purus tincidunt sed. Cras consequat congue lectus, eget dignissim est vestibulum ut.
+
+  Vivamus orci neque, rhoncus et vehicula ut, placerat eget nisi. Vivamus commodo pellentesque pulvinar. Curabitur suscipit ante lectus, sed auctor ipsum congue eu. Fusce dictum nibh nec neque dapibus pretium. In sed aliquam ante, non pellentesque elit. Ut in velit massa. Ut odio nulla, elementum fringilla elementum vitae, suscipit ac justo. Cras et tincidunt augue, eget imperdiet eros. Aliquam sed tincidunt ante. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Aenean eu est bibendum, dictum tortor a, consectetur diam. Mauris enim orci, egestas ut nibh a, imperdiet egestas purus. Vestibulum at feugiat diam, et commodo mauris.
+
+  Sed vitae pellentesque sapien. Maecenas vel volutpat elit. Aenean nisi tellus, pharetra eget est nec, interdum posuere erat. Etiam vestibulum arcu eros, sit amet facilisis urna consectetur sed. Nunc sodales fringilla augue, malesuada condimentum mi commodo ut. Quisque tempor auctor lectus, a porttitor augue volutpat a. Sed elementum euismod congue. Quisque tellus arcu, mollis at varius eu, gravida quis tortor. Mauris sit amet orci ante. Pellentesque hendrerit velit eu risus maximus, at tincidunt dolor fermentum.</p>
+        </div>
+      </>
     )
 }
 
--- client/ReactProject/src/SharedComponents/Footer/FooterMain.js
+++ client/ReactProject/src/SharedComponents/Footer/FooterMain.js
@@ -5,11 +5,11 @@ import FooterContent from './FooterContent';
 
 export default function FooterMain(){
     return (
-        <div>
+        <>
             <FooterMenu />
             <FooterMenuMobile />
             <FooterContent />
             <div id="copyright-background">©Copyright ©2020 Sungent Packaging | All rights reserved</div>
-        </div>
+        </>
     );
   }
