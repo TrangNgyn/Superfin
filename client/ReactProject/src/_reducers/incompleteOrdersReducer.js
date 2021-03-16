d75d42075af5d582e6c1506e2cd33263f3761e19
steamedHams11 <69377385+steamedHams11@users.noreply.github.com>
2021-03-16 18:27:08 +1000

    Finished admin homepage order API

--- client/ReactProject/src/_reducers/incompleteOrdersReducer.js
+++ client/ReactProject/src/_reducers/incompleteOrdersReducer.js
@@ -1,4 +1,5 @@
-import { GET_INCOMPLETE_ORDERS, ERROR_INCOMPLETE_ORDERS, LOADING_INCOMPLETE_ORDERS } from '../_actions/actionTypes';
+import { GET_INCOMPLETE_ORDERS, ERROR_INCOMPLETE_ORDERS, LOADING_INCOMPLETE_ORDERS, DELETE_INCOMPLETE_ORDERS, ADD_TRACKING } from '../_actions/actionTypes';
+import { orderStatusConstants } from '../_constants/orderStatus.constants'; 
 
 const initState = {
     incompleteOrders: [],
@@ -11,7 +12,7 @@ const incompleteOrdersReducer = (state = initState, {type, payload}) => {
         case GET_INCOMPLETE_ORDERS:
             return {
                 ...state,
-                incompleteOrders: payload,
+                incompleteOrders: payload.sort((a,b) => { return new Date(b.issued_date) - new Date(a.issued_date)}),
                 loading: false,
                 error: false
             }
@@ -25,6 +26,24 @@ const incompleteOrdersReducer = (state = initState, {type, payload}) => {
             return {
                 ...state,
                 loading: payload
+            }
+        case DELETE_INCOMPLETE_ORDERS:
+            return {
+                ...state,
+                incompleteOrders: state.incompleteOrders.filter(o => o.po_number !== payload)
+            }
+        case ADD_TRACKING:
+            const index = state.incompleteOrders.findIndex(o => { return o.po_number === payload.po_number });
+            const tempArr = state.incompleteOrders;
+            let tempObj = tempArr[index];
+            
+            tempObj.tracking_number = payload.tracking_number;
+            tempObj.carrier = payload.carrier;
+            tempObj.status = orderStatusConstants.SHIPPED;
+
+            return {
+                ...state,
+                incompleteOrders: [...tempArr]
             }
         default:
             return state
