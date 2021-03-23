d4a0e2a02b80bbbee3b2bc0d122e75ad4609feb6
steamedHams11 <69377385+steamedHams11@users.noreply.github.com>
2021-03-23 23:17:37 +1000

    Adding links to categories

--- client/ReactProject/src/Pages/Homepage/Category.js
+++ client/ReactProject/src/Pages/Homepage/Category.js
@@ -1,11 +1,12 @@
 import '../../_assets/CSS/pages/Homepage/Category.css';
 import altImage from "../../_assets/Images/No_Image.jpg"
+import { history } from '../../_helpers/history';
 
 const Category = props => {
     const category  = props;
    
     return (
-        <div className="Category-Container">
+        <div className="Category-Container" onClick = {() => { history.push(`products/${category.c_name}`)}}>
             <img className = "Category-Image-Text"
                 src= {category.c_image}
                 alt={altImage}
