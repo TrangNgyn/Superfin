264902a28270997c743d59cd250b8a389c616e7a
kathniev <chieuvienng@gmail.com>
2021-03-28 18:03:07 +0700

    product pages

--- client/ReactProject/src/Pages/AllProducts/AllProducts.js
+++ client/ReactProject/src/Pages/AllProducts/AllProducts.js
@@ -1,15 +1,61 @@
-import ProductList from '../../SharedComponents/ProductList/ProductList';
-import '../../_assets/CSS/pages/Homepage/Homepage.css';
+import '../../_assets/CSS/pages/Homepage/CategoryList.css';
+import { getAllProducts } from '../../_actions/productActions';
+import { useState, useEffect } from "react";
+import { Pagination, Spin, Card } from 'antd';
+import { useDispatch, useSelector } from 'react-redux';
 
-export default function AllProducts(){
-    return(
-    	<>
-	        <div className="homepage-product-list-outer-container-0">
-            <h3>All Product</h3>
-                <div id="homepage-product-list-outer-container-1">
-                    <ProductList />
-                </div>
-	        </div>
+const itemsPerPage = 12;
+
+const AllProducts = () => {
+    const dispatch = useDispatch(); // In charge of getting data from productActions.
+
+    const [page, setPage] = useState(0);
+
+    const products = useSelector(state => state.productState.products);
+    const productsLoading = useSelector(state => state.productState.productsLoading);
+    const productsError = useSelector(state => state.productState.productsError);
+
+    const maxNumberOfPages = (Math.ceil(products.length/itemsPerPage) - 1);
+
+    const productsList = products.slice( page * itemsPerPage,
+        ((page + 1) * itemsPerPage) > products.length ? products.length : ((page + 1) * itemsPerPage));
+
+    const productView = productsList.map((item, index) => {
+        return (
+        <div key={index}>
+            <Card
+                hoverable
+                style={{ width: 120 }}
+                cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+            >
+                <Card title={item.p_name} description={item.p_code} />
+            </Card>
+        </div>);
+    });
+    useEffect(() => {         
+        if(!products.length){
+            dispatch(getAllProducts());
+        }
+    }, [products.length, dispatch]);
+
+    const onChange = p => { setPage(p - 1) };
+
+    return (
+        <>
+            {
+                productsError 
+                ?   <h1 style = {{textAlign: 'center', color: 'red'}}>Could not load data, please try refreshing page</h1>:<></>
+            }
+            {
+                productsLoading
+                ?   <Spin size='large'/> 
+                :   <>
+                        <div className="Category-List-Container">{productView}</div>
+                        <Pagination defaultCurrent={1} total={(maxNumberOfPages + 1) * 10} onChange = {onChange}/>
+                    </>  
+            }  
         </>
     );
-}
+};
+
+export default AllProducts;
---
+++ client/ReactProject/src/SharedComponents/ProductList/ProductList.js
@@ -0,0 +1,71 @@
+import React, { useState } from "react";
+// // import Product from '../Product/Product';
+// // import {fetchProducts} from '../ProductList/MockProductList';
+// import '../../_constants';
+// import { pageConstants } from "../../_constants";
+// import { Pagination } from 'antd';
+// import { Link } from 'react-router-dom';
+
+
+
+
+
+// const currentPage = pageConstants.HOMEPAGE_CUSTOMER; //currentPage will get initialized using Redux later
+
+// const prods = fetchProducts();
+
+// function onlyCategories(value, index, self){
+//   return self.map(function(e) {
+//     return e.category;
+//   }).indexOf(value.category) === index;
+// }
+
+
+
+
+
+
+// const ProductList = () => {
+//     const [page, setPage] = useState(0);
+//     const onChange = p => { setPage(p - 1) };
+
+//     //const [isLoading, setIsLoading] = useState(true);
+//     const itemsPerPage = 6;
+//     let productsArray = null;
+
+
+
+
+
+
+//     if(currentPage === pageConstants.HOMEPAGE_CUSTOMER) productsArray = prods.filter(onlyCategories);
+//     else productsArray = prods
+
+//     const maxNumberOfPages = (Math.ceil(productsArray.length/itemsPerPage) - 1);
+
+//     let renderableArray = productsArray.slice( page * itemsPerPage,
+//       ((page + 1) * itemsPerPage) > productsArray.length ? productsArray.length : ((page + 1) * itemsPerPage));
+
+//     const rederableProducts = renderableArray.map((p) => {
+//       return <div key={p.id}><Link to="/ViewProductInfo"><Product {...p}/></Link></div>
+//     });
+
+
+
+
+
+
+
+//     return (
+//       <>
+//         <div className="Product-List-Container">
+        
+
+//           {rederableProducts}
+//         </div>
+//         <Pagination defaultCurrent={1} total={(maxNumberOfPages + 1) * 10} onChange = {onChange}/>
+//       </>
+//   );
+// };
+
+// export default ProductList;
