92224677cf4c8eaf9be827080e55ea4d3914a4f2
Oliver <55520829+od948@users.noreply.github.com>
2021-05-07 13:33:26 +1000

    Removed Disk Storage

--- server/middleware/upload.js
+++ server/middleware/upload.js
@@ -10,16 +10,6 @@ aws.config.update({
     region: 'ap-southeast-2'
 })
 
-var storage = multer.diskStorage({
-    destination: function(req, file, cb) {
-      cb(null, 'uploads')
-    },
-    filename: function(req, file, cb) {
-      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-      cb(null, uniqueSuffix);
-    }
-  })
-
 var upload = multer({
     fileFilter: function (req, file, cb) {
         if(file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
