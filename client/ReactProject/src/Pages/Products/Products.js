bfb326d5a94d81044e1685c7cb8f11c4d1cbbb39
kathniev-source <kathrynviennguyen@gmail.com>
2021-05-11 19:19:18 +1000

    Make Category selection a treeselect to show hierarchy

--- client/ReactProject/src/Pages/Products/Products.js
+++ client/ReactProject/src/Pages/Products/Products.js
@@ -1,4 +1,4 @@
-import { Spin, Select } from 'antd';
+import { Spin, Select, TreeSelect} from 'antd';
 import { SortDescendingOutlined, SortAscendingOutlined, FallOutlined, RiseOutlined } from '@ant-design/icons';
 import { useState, useEffect } from "react";
 import { useDispatch, useSelector } from 'react-redux';
@@ -6,6 +6,7 @@ import ProductsList from './ProductsList';
 import { getAllCategories } from '../../_actions/categoryActions';
 import { getAllProducts } from '../../_actions/productActions';
 const { Option } = Select;
+const { TreeNode } = TreeSelect;
 
 const Products = () => {
     const dispatch = useDispatch();
@@ -37,12 +38,13 @@ const Products = () => {
         }
     }
 
-    const addSubCategories = (parents, categoriesList, indent) => {
+    const addSubCategories = (parents, categoriesList) => {
         if (categoriesList == null) {
             console.error("Sorry, categories list is null!")
         } else {
             parents.forEach(parent => {
-                parent.indent = indent;
+                parent.title = parent.c_name;
+                parent.value = parent._id;
                 parent.children = [];
                 var childrenList = null;
                 childrenList = categoriesList.filter(category => category.path.includes(parent.c_name)); // Filter the children out of the list
@@ -50,7 +52,7 @@ const Products = () => {
                     categoriesList.splice(categoriesList.indexOf(child), 1); // Remove the children from the categories list
                     parent.children.push(child);
                 });
-                addSubCategories(parent.children, parent.children, indent + 1);
+                addSubCategories(parent.children, parent.children);
             });
         }
     }
@@ -59,25 +61,12 @@ const Products = () => {
         var copyOfCategories = categories.map(x => x);
         const rootParents = copyOfCategories.filter(category => category.path == null);
         rootParents.forEach(parent => { copyOfCategories.splice(copyOfCategories.indexOf(parent), 1); });
-        addSubCategories(rootParents, copyOfCategories, 0);
-        var flattened = flattenHierarchy(rootParents);
-        return flattened;
-    }
-
-    function flattenHierarchy(hierarchy) {
-        var flat = [];
-        hierarchy.forEach((node) => {
-            flat.push({
-                _id: node._id,
-                c_name: node.c_name,
-                indent: node.indent
-            }); // Push the parent in first
-            if(node.children.length != 0){
-                // Recursively add everything by calling function on the children list if it's not null
-                flat.push(flattenHierarchy(node.children));
-            }
-        });
-        return flat.flat();
+        addSubCategories(rootParents, copyOfCategories);
+        return([{
+            title: 'All Categories',
+            value: 'allCategories',
+            children: rootParents
+        }]);
     }
 
     const filterAndSortProduct = () => {
@@ -106,17 +95,7 @@ const Products = () => {
             { error ? <div class="container"><h1 style={{ textAlign: 'center', color: 'red' }}>Could not load data, please try refreshing page!</h1></div> :
                 (loading ? <Spin size='large' /> : <>
                     <div className="container flex-horizontal-box-container">
-                        <Select className="box-item-xs-2 box-item-sm-3 box-item-md-3 box-item-lg-3 box-item-xl-4" defaultValue="allCategories" onSelect={e => { setFilter(e); }}>
-                            <Option value="noCategory" disabled>Filter By Category</Option>
-                            <Option value="allCategories" >All Category</Option>
-                            {
-                                // <Option value={category._id}>{spaces.join("") + category.c_name}</Option>
-                                getCategoriesHierarchy(categories).map((category, index) => {
-                                    return <Option key={index} value={category._id}>{new Array(category.indent).fill('\u2003').join('') + category.c_name}</Option>;
-                                })
-                            }
-
-                        </Select>
+                        <TreeSelect className="box-item-xs-2 box-item-sm-3 box-item-md-3 box-item-lg-3 box-item-xl-4" treeData={getCategoriesHierarchy(categories)} placeholder="Filter Category" defaultValue="allCategories" treeDefaultExpandAll onSelect={e => { setFilter(e); }}/>
                         <Select className="box-item-xs-2 box-item-sm-3 box-item-md-2 box-item-lg-3 box-item-xl-4" defaultValue="priceAsc" onSelect={e => { setSorted(e); }}>
                             <Option value="noSort" disabled><SortAscendingOutlined />Sort By</Option>
                             <Option value="alphaAsc"><SortAscendingOutlined />Sort By: Alphabetically Ascending</Option>
