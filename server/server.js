e68c12984e19604eab4406a52f2dff9d7fd02480
Oliver <55520829+od948@users.noreply.github.com>
2021-05-17 22:40:30 +1000

    Update server.js

--- server/server.js
+++ server/server.js
@@ -18,7 +18,8 @@ const about = require('./routes/api/aboutus');
 const app = express();
 
 // bodyparser middleware
-app.use(bodyParser.json());
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 // connect to mongo
 mongoose.connect(process.env.mongoURI, 
