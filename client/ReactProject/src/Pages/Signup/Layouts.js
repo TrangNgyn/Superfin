e9d4d09f7a44da76ba349f6e5abfa7bf3be391e6
steamedHams11 <69377385+steamedHams11@users.noreply.github.com>
2021-05-21 15:29:03 +1000

    Sign up finished and tested

---
+++ client/ReactProject/src/Pages/Signup/Layouts.js
@@ -0,0 +1,18 @@
+
+export const layout2 = {
+    labelCol: {
+        span: 8,
+    },
+    wrapperCol: {
+      span: 7,
+    },
+};
+
+
+  
+export const tailLayout = {
+    wrapperCol: {
+        offset: 8,
+        span: 16,
+    }
+};
